39a90f1a73cf160741d6a877e8066af01f202122
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2020-10-14 17:01:04 +0300

    Add schema stub

---
+++ application/schemas/.database.js
@@ -0,0 +1,24 @@
+({
+  name: 'jsl',
+  description: 'Just Save Life',
+  version: 1,
+  driver: 'pg',
+
+  authors: [
+    { name: 'Timur Shemsedinov', email: 'timur.shemsedinov@gmail.com' },
+  ],
+
+  extensions: [
+    'hstore',
+    'postgis',
+    'postgis_topology',
+  ],
+
+  connection: {
+    host: '127.0.0.1',
+    port: 5432,
+    database: 'application',
+    user: 'postgres',
+    password: 'postgres',
+  },
+});
