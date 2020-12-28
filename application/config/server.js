3b2107028aec2566c6aa08c8bec663230f75814c
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2020-12-28 18:34:34 +0200

    Bind all network interfacer at :80 + :443

--- application/config/server.js
+++ application/config/server.js
@@ -1,16 +1,12 @@
 ({
-  host: '127.0.0.1',
-  balancer: 8000,
-  protocol: 'http',
-  ports: [8001, 8002],
+  host: '0.0.0.0',
+  balancer: 80,
+  protocol: 'https',
+  ports: [443],
   timeout: 5000,
   concurrency: 1000,
   queue: {
     size: 2000,
     timeout: 3000,
-  },
-  workers: {
-    pool: 2,
-    timeout: 3000,
-  },
+  }
 });
