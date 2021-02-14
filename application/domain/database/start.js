235f63a768f3e176a3a1b6a384a1cc225e4c2214
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2021-02-14 23:49:06 +0200

    Optimize database init

--- application/domain/database/start.js
+++ application/domain/database/start.js
@@ -1,6 +1,9 @@
-async () => {
-  console.debug('Connect to pg');
-  const database = new metarhia.metasql.Database(config.database);
+(async () => {
+  if (application.worker.id === 'W1') {
+    console.debug('Connect to pg');
+  }
+  const options = { ...config.database, logger: console };
+  const database = new metarhia.metasql.Database(options);
   domain.db = database;
   application.auth.init(database);
-};
+});
